5978142c5ace42c00a82f711d2a4c1769731adf2
aaronvark <aoostdijk@aarons-macbook-pro.local>
2016-10-11 16:40:47 +0200

    Initial Commit

---
+++ o2_test_forked/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  o2_test_forked
+//
+//  Created by Aaron Oostdijk on 11/10/16.
+//  Copyright © 2016 Call 0031646001602. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
